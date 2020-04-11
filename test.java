c8974c21dea1d781a925007aad90b0e37c10a9ec
Ananan <1002263692@qq.com>
2020-04-11 21:03:33 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+package com.jackiifilwhh.services;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str1 = "abcdefg";
+		String str2 = "abcde    d";
+		int i = 0;
+		System.out.println(str1.compareTo(str2));
+	}
+}
