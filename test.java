e77e978cc1ed45ae3d614ccf422fe10a85014014
Ananan <1002263692@qq.com>
2020-04-11 21:04:11 +0800

    Update test.java

--- test.java
+++ test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		String str1 = "abcdefg";
 		String str2 = "abcde    d";
-		int i = 0;
-		System.out.println(str1.compareTo(str2));
+		String str2 = "abcde    d";
+		String str2 = "abcde    d";
+		int i = 5;
 	}
 }
